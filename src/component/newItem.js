83fc2c2fd2ff6a3bfb77671c48987238d2d5b656
drbarq <J.Tustin@gmail.com>
2019-07-23 11:14:43 -0600

    added styling

--- src/component/newItem.js
+++ src/component/newItem.js
@@ -23,13 +23,43 @@ export default class NewItems extends Component {
                             <p>new item</p>
                             <button>create new item</button>
                         </div>
-                        <div className="newItemInfoContainer">
-                            <div className="newItemNameContainer">
-                                <label htmlFor="newItemName">name</label>
-                                <input className="newItemName" type="text"></input>
+                        <form className="newItemInfoContainer" action="" method="post">
+                            <div className="newItemContainerRowOne">
+                                <div className="newItemNameContainer">
+                                    <label htmlFor="newItemName">name</label>
+                                    <input className="newItemName" type="text"></input>
+                                </div>
+                                <div className="qrCodeContainer">
+                                    <img className="qrcode" src="https://lh3.googleusercontent.com/JhlUkFmf_qYUMEV_H-WPvG_TMB7oiSY8jqqRIkzJ_cZRe0eWYIx_x59NPMX-gMpUSRZN=s128"/>
+                                </div>
+
+                            </div>
+                            <div className="newCommsContainer">
+                                <div className="newCommsOptionContainer">
+                                    <div className="newTitleContainer">
+                                        <label htmlFor="newPhoneComms">phone</label>
+                                        <input className="newPhoneCheck" type="checkbox"></input>
+                                    </div>
+                                    <input className="newPhoneComms" type="text"></input>
+                                </div>
+                                <div className="newCommsOptionContainer">
+                                    <div className="newTitleContainer">
+                                        <label htmlFor="newTextComms">text</label>
+                                        <input className="newTextCheck" type="checkbox"></input>
+                                    </div>
+                                    <input className="newTextComms" type="text"></input>
+                                </div>
+                                <div className="newCommsOptionContainer">
+                                    <div className="newTitleContainer">
+                                        <label htmlFor="newEmailComms">email</label>
+                                        <input className="newTextCheck" type="checkbox"></input>
+                                    </div>
+                                    <input className="newEmailComms" type="text"></input>
+                                </div>
+
                             </div>
 
-                        </div>
+                        </form>
                     </div>
                 </div>
             </body>
