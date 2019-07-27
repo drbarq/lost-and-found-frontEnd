4d8c70f554c20b15815e7815743108640f61cacb
drbarq <J.Tustin@gmail.com>
2019-07-27 16:21:42 -0600

    added style and flow

--- src/component/viewUser.js
+++ src/component/viewUser.js
@@ -1,14 +1,24 @@
 import React, { Component} from 'react'
+import { Redirect } from 'react-router-dom'
 import '../css/viewUser.css'
 
 
 export default class ViewUser extends Component {
     constructor() {
         super()
-        this.state = {}
+        this.state = {
+            inEditUser: false
+        }
+    }
+
+    handleEditUserClick = () => {
+        this.setState({inEditUser: true})
     }
 
     render(props) {
+        if (this.state.inEditUser === true) {
+            return <Redirect to='/editUser/' />
+        }
         return(
             <React.Fragment>
                 <div className="viewUser-viewUserContainer">
@@ -26,11 +36,11 @@ export default class ViewUser extends Component {
                             <div className="viewUser-viewNameContainer">
                                 <div className="viewUser-firstName">
                                     <label htmlFor="viewUser-viewFirstName">first name</label>
-                                    <input className="iewUser-viewFirstName" type="text" defaultValue={this.props.userInfo.firstName}></input>
+                                    <input className="iewUser-viewFirstName" type="text" defaultValue={this.props.userInfo.firstName} onClick={this.handleEditUserClick}></input>
                                 </div>
                                 <div className="viewUser-lastName">
                                     <label htmlFor="viewUser-viewLastName">last name</label>
-                                    <input className="viewUser-viewLastName" type="text" defaultValue={this.props.userInfo.lastName}></input>
+                                    <input className="viewUser-viewLastName" type="text" defaultValue={this.props.userInfo.lastName} onClick={this.handleEditUserClick}></input>
                                 </div>
                             </div>
                         </div>
@@ -38,14 +48,13 @@ export default class ViewUser extends Component {
                             <div className="viewUser-viewUserCommOptionContainer">
                                 <div className="viewUser-viewTitleContainer">
                                     <label htmlFor="viewUser-viewUserPhone">phone</label>
+                                    <input className="viewUser-viewUserPhone" type="text" defaultValue={this.props.userInfo.contactNumber} onClick={this.handleEditUserClick}></input>
                                 </div>
-                                <input className="viewUser-viewUserPhone" type="text"></input>
-                            </div>
-                            <div className="viewUser-viewUserCommOptionContainer">
                                 <div className="viewUser-viewTitleContainer">
                                     <label htmlFor="viewUser-viewUserEmail">email</label>
+                                    <input className="viewUser-viewUserEmail" type="text" defaultValue={this.props.userInfo.emailAddress} onClick={this.handleEditUserClick}></input>
                                 </div>
-                                <input className="viewUser-viewUserEmail" type="text"></input>
+                                
                             </div>
                         </div>
                     </form>
